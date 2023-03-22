5f6d2bdff4cb4ac94206e5cbdf7211738658fc56
Tolga Pirim <tolgapirim25@gmail.com>
2023-03-22 12:53:51 +0300

    Implemented GenderScreen.kt

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -14,8 +14,11 @@ import androidx.navigation.compose.rememberNavController
 import com.plcoding.calorytracker.navigation.navigate
 import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
 import com.plcoding.core.navigation.Route
+import com.plcoding.onboarding_presentation.gender.GenderScreen
 import com.plcoding.onboarding_presentation.welcome.WelcomeScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,15 +32,15 @@ class MainActivity : ComponentActivity() {
                 ) {
                     composable(Route.WELCOME) {
                         WelcomeScreen(
-                            onNavigate =navController::navigate
+                            onNavigate = navController::navigate
                         )
                     }
 
-                    composable(Route.AGE) {
-
+                    composable(Route.GENDER) {
+                        GenderScreen(onNavigate = navController::navigate)
                     }
 
-                    composable(Route.GENDER) {
+                    composable(Route.AGE) {
 
                     }
 
