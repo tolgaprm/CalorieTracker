d3153cccb4055882320ed2c3fc433fc1c373afde
Tolga Pirim <tolgapirim25@gmail.com>
2023-03-24 16:04:29 +0300

    setting up the Local DB

--- tracker/tracker_data/src/main/java/com/plcoding/tracker_data/di/TrackerDataModule.kt
+++ tracker/tracker_data/src/main/java/com/plcoding/tracker_data/di/TrackerDataModule.kt
@@ -1,5 +1,8 @@
 package com.plcoding.tracker_data.di
 
+import android.app.Application
+import androidx.room.Room
+import com.plcoding.tracker_data.local.TrackerDatabase
 import com.plcoding.tracker_data.remote.OpenFoodApi
 import dagger.Module
 import dagger.Provides
@@ -37,5 +40,17 @@ object TrackerDataModule {
             .client(client)
             .build()
             .create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTrackerDatabase(
+        app: Application,
+    ): TrackerDatabase {
+        return Room.databaseBuilder(
+            context = app,
+            klass = TrackerDatabase::class.java,
+            name = "tracker_db"
+        ).build()
     }
 }
